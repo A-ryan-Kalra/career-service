81eb3a9f2561a9b9b07632a11e616d5f18304c77
Aryan Kalra <aryan.smart832@gmail.com>
2026-02-06 17:25:12 +0530

    add rate controller

---
+++ src/controller/rate.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { RateRequestSchema } from "../models/rate-request";
+import { getRates } from "../service/rate.service";
+
+export async function rateController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    console.log("first");
+    const parsed = RateRequestSchema.parse(req.body);
+    const rates = await getRates(parsed);
+    res.json(rates);
+  } catch (err) {
+    next(err);
+  }
+}
